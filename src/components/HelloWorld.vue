77dfb04be6a57d9592d42b51842a39948f12e7f8
Valeria <valeriahernandezgutierrezluzac@gmail.com>
2025-02-10 18:51:24 -0600

    adición de tema de tailwind, colores y tipografía

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,9 +5,10 @@ defineProps({
 </script>
 
 <template>
-  <div class="text-5xl">
+  <div class="text-5xl font-inter text-dark-violet">
     Holaaaaaa test
   </div>
+  <p class="text-neon-pink text-2xl">Color?</p>
 </template>
 
 <style scoped>
