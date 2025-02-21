6bff2c1ef3182dea6e9406cf225f5c766b4cf783
Ritoyy <yassergf@outlook.com>
2025-02-21 13:07:47 -0600

    adicion de boton mi carrito

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -21,7 +21,9 @@
                 <div class="dropdown-content text-white p-2 font-inter bg-navy rounded-xl">
                     <p @click="router.push('/mis-disenos')">Mis Diseños</p>
                     <p>Mis Pedidos</p>
+                    <p @click="router.push('/carrito')">Mi Carrito</p>
                     <p @click="router.push('/login')">Iniciar sesión</p>
+                    
                 </div>
             </div>
 
