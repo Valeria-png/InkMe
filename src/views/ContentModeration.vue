9011caac453ff1933c655626bc61239d4cca314f
Valeria <valeriahernandezgutierrezluzac@gmail.com>
2025-03-18 14:29:16 -0600

    funcionamiento de moderacion de contenido

--- src/views/ContentModeration.vue
+++ src/views/ContentModeration.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="flex items-center space-x-4 mb-6">
       <router-link to="/admin">
-       <img src="/public/inkme logo.png" alt="Logo Inkme" class="w-35" />
+       <img src="/public/inkme logo.png" alt="Logo Inkme" class="w-50" />
       </router-link>
       <div class="flex space-x-2 overflow-auto">
-        <div v-for="n in 25" :key="n" class="min-w-14 h-14 bg-gray-300 rounded"></div>
+        <div @click="currentDesign = designs" v-for="designs in designsToApprove" class="min-w-20 h-24 bg-gray-300 rounded cursor-pointer">
+          <img  :src="designs.file" class="w-full h-full object-cover" alt="">
+        </div>
       </div>
     </div>
     <div class="p-6 max-w-4xl mx-auto rounded-lg">
       <div class="grid grid-cols-3 gap-4">
         <!-- Imagen principal -->
         <div class="col-span-1 bg-gray-300 h-80 rounded-md overflow-hidden">
-          <img v-if="mainImage" :src="mainImage" alt="" class="w-full h-full object-cover">
+          <img  :src="currentDesign.file" alt="" class="w-full h-full object-cover">
+          
         </div>
         
         <!-- Información del diseño -->
@@ -20,7 +23,7 @@
             <label class="block text-gray-600">Título del diseño</label>
             <input 
               type="text" 
-              v-model="title" 
+              v-model="currentDesign.name" 
               class="w-full border p-2 rounded-xl text-dark-pink font-bold"
               :readonly="!isEditable"
             >
@@ -29,7 +32,7 @@
           <div>
             <label class="block text-gray-600">Descripción del diseño</label>
             <textarea 
-              v-model="description" 
+              v-model="currentDesign.description" 
               class="w-full border p-2 rounded-xl text-navy"
               :readonly="!isEditable"
             ></textarea>
@@ -43,32 +46,65 @@
           </button>
   
           <div class="flex justify-between mt-6">
-            <button class="bg-neon-pink text-white px-6 py-2 rounded-xl cursor-pointer hover:scale-105 transition">Aprobar Diseño</button>
-            <button class="bg-dark-pink text-white px-6 py-2 rounded-xl cursor-pointer hover:scale-105 transition">Desaprobar Diseño</button>
+            <button @click="updateDesign(true)" class="bg-neon-pink text-white px-6 py-2 rounded-xl cursor-pointer hover:scale-105 transition">Aprobar Diseño</button>
+            <button @click="updateDesign(false)" class="bg-dark-pink text-white px-6 py-2 rounded-xl cursor-pointer hover:scale-105 transition">Desaprobar Diseño</button>
           </div>
         </div>
       </div>
     </div>
   </template>
   
-  <script>
+  <script setup>
+import { ref, onMounted } from 'vue';
 
-  export default {
-    data() {
-      return {
-        title: "Nombre Título de Diseño Subido",
-        description: "Descripción del diseño subido por el usuario",
-        mainImage: "https://via.placeholder.com/300", // Imagen principal de ejemplo
-        isEditable: false, // Controla si los campos son editables
-      };
-    },
-    methods: {
-      toggleEditable() {
-        this.isEditable = !this.isEditable; // Cambia el estado de edición
-      }
-    }
-  };
-  </script>
+const isEditable = ref(false);
+const designsToApprove = ref([]);
+const currentDesign = ref({});
+
+
+const toggleEditable = () => {
+  isEditable.value = !isEditable.value; // Cambia el estado de edición
+};
+
+ //api requests
+
+ const updateDesign = async (isApprovedBool) => {
+  console.log(currentDesign.value.name)
+  const newName = currentDesign.value.name;
+  const newDescription = currentDesign.value.description;
+  console.log('voyaqui')
+   const response = await fetch (`https://inkmeapi.onrender.com/api/designs/${currentDesign.value._id}`, 
+   {
+     method: "PUT",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body:JSON.stringify({
+       name: newName,
+       description: newDescription,
+       isApproved: isApprovedBool,
+       file: currentDesign.value.file,
+       user_id: currentDesign.value.user_id,
+       added_value: currentDesign.value.added_value,
+       text: currentDesign.value.text ,
+       text_color: currentDesign.value.text_color
+     })
+   })
+   const data = await response.json()
+   console.log(data)
+   designsToApprove.value.splice(0,1);
+ }
+ const getDesignsToApprove = async () => {
+   const response = await fetch ("https://inkmeapi.onrender.com/api/designs")
+   const data = await response.json();
+   designsToApprove.value = data.filter(design => design.isApproved === false );
+   currentDesign.value = designsToApprove.value[0];
+ }
+
+onMounted(() => {
+  getDesignsToApprove()
+});
+</script>
   
   <style scoped>
   </style>
