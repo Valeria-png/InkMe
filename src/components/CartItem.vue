7ac2cf1f14aed622ab8e507c7d562bc98468f686
Ritoyy <yassergf@outlook.com>
2025-03-25 13:00:03 -0600

    Fix valor añadido agregado al carrito y post de pedidos

--- src/components/CartItem.vue
+++ src/components/CartItem.vue
@@ -1,3 +1,31 @@
+<script setup>
+import { ref, watch } from 'vue';
+
+const props = defineProps({ item: Object });
+const emit = defineEmits(['update', 'remove']);
+
+const localQuantity = ref(props.item.quantity);
+
+watch(localQuantity, (newQuantity) => {
+    if (newQuantity < 1) localQuantity.value = 1;
+    emit('update', { ...props.item, quantity: localQuantity.value });
+});
+
+const decreaseQuantity = () => {
+    if (localQuantity.value > 1) {
+        localQuantity.value--;
+    }
+};
+
+const increaseQuantity = () => {
+    localQuantity.value++;
+};
+
+const recalculate = () => {
+    if (localQuantity.value < 1) localQuantity.value = 1;
+};
+</script>
+
 <template>
     <div class="grid grid-cols-6">
         <span class="p-1">
@@ -9,6 +37,7 @@
                 <h1 class="text-dark-violet font-inter font-semibold md:text-3xl sm:text-2xl">{{ item.name }}</h1>
                 <p class="text-neon-pink font-bagel-fat-one md:text-3xl sm:text-2xl">${{ item.price }}</p>
                 <p class="text-neon-pink font-inter md:text-xl sm:text-lg">${{ item.unitPrice }} cada uno</p>
+                <p class="text-dark-violet font-inter md:text-xl sm:text-lg">Valor añadido: ${{ item.added_value }}</p>
                 <p class="text-dark-violet font-inter md:text-xl sm:text-lg">{{ item.level }}</p>
             </div>
         </span>
@@ -27,43 +56,12 @@
                     <button class="text-white text-2xl font-bold cursor-pointer" @click="increaseQuantity">+</button>
                 </div>
 
-                <button 
-    class="h-3/5 bg-dark-pink text-white rounded-3xl font-inter sm:text-lg md:text-xl lg:text-2xl hover:scale-105 transform cursor-pointer" 
-    @click="$emit('remove', item.designedproduct_id, item.cartId)">
-    Eliminar
-</button>
-
+                <button class="h-3/5 bg-dark-pink text-white rounded-3xl font-inter sm:text-lg md:text-xl lg:text-2xl hover:scale-105 transform cursor-pointer" 
+                    @click="$emit('remove', item.designedproduct_id, item.cartId)">
+                    Eliminar
+                </button>
             </div>
         </span>
     </div>
     <hr class="border-t-2">
 </template>
-
-<script setup>
-import {  ref, watch } from 'vue';
-
-const props = defineProps({ item: Object });
-const emit = defineEmits(['update', 'remove']);
-
-const localQuantity = ref(props.item.quantity);
-
-watch(localQuantity, (newQuantity) => {
-    if (newQuantity < 1) localQuantity.value = 1;
-    emit('update', { ...props.item, quantity: localQuantity.value });
-});
-
-
-const decreaseQuantity = () => {
-    if (localQuantity.value > 1) {
-        localQuantity.value--;
-    }
-};
-
-const increaseQuantity = () => {
-    localQuantity.value++;
-};
-
-const recalculate = () => {
-    if (localQuantity.value < 1) localQuantity.value = 1;
-};
-</script>
